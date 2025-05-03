9cfa6ca5e196a4652193cfa699d3777facf4a5f0
Miguel Angel López Monroy <miguel.lopezm.dev@gmail.com>
2025-05-03 15:48:02 -0600

    Initial commit

---
+++ tests/unit/test_serverless_ecommerce_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from serverless_ecommerce.serverless_ecommerce_stack import ServerlessEcommerceStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in serverless_ecommerce/serverless_ecommerce_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = ServerlessEcommerceStack(app, "serverless-ecommerce")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
